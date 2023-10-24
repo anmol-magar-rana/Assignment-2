fbc19319060167c85c8e7bebc44b4b4be6fe7fdf
anmol-magar-rana <148839281+anmol-magar-rana@users.noreply.github.com>
2023-10-23 23:34:18 -0600

    Update logic.js

---
+++ Assignment 2/logic.js
@@ -0,0 +1,34 @@
+//function turn()
+//every time a piece makes a legal move, the box below updates whose turn it is
+
+//function reset()
+//When clicked on, resets the pieces and graveyard to original place
+
+//function color_box() 
+//when the pieces are clicked on,
+//this function will color the boxes that the pieces can move to (legal moves only), including valid captures
+
+//function insert_image()
+//when click on piece and a legal move is made with that piece, picture updates
+
+//function check_move()
+//function checks if a move is legal or not
+//illegal eg. capture own pieces, wrong movements of the unique pieces, king is in check etc
+
+//function king_check()
+//function checks if a players king is in check after a legal move
+
+//function checkmate()
+//checks for checkmate after every move 
+
+//function graveyard()
+//updates the graveyard after every capture
+
+//function promote()
+//when pawns reach the endzone they can promote to one of each piece: Queen, Bishop, Knight, Rook
+
+//function movement()
+//calculates the movement of each selected piece
+
+//function win()
+//victory message for the player who wins after checkmate
